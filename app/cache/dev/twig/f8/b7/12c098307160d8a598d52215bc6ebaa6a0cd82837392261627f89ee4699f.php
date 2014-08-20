d0de9516c5f16afabfac9aa0c08d22e371aaaa90
Mihai Racu <mracu@yopeso.com>
2014-08-20 16:26:15 +0300

    Added app folder

---
+++ app/cache/dev/twig/f8/b7/12c098307160d8a598d52215bc6ebaa6a0cd82837392261627f89ee4699f.php
@@ -0,0 +1,39 @@
+<?php
+
+/* MySiteBundle:Default:index.html.twig */
+class __TwigTemplate_f8b712c098307160d8a598d52215bc6ebaa6a0cd82837392261627f89ee4699f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "MySiteBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
